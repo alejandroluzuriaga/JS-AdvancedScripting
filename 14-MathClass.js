0503d5c5f94f64f9313414e413c1141eeb1a1298
alejandroluzuriaga <luzuriagalejandro@gmail.com>
2022-11-30 12:12:08 +0100

    14-MathClass.js

---
+++ 14-MathClass.js
@@ -0,0 +1,8 @@
+//Principal methods of math class:
+
+console.log(Math.random()); //pseudorandom number between 0 and 1
+console.log(Math.ceil(0.4)) //round to closest integer number - UP
+console.log(Math.floor(0.7)) //round to closest integer number - DOWN
+console.log(Math.round(0.8)) //round to closest integer number
+
+console.log(Math.random().toFixed(2)) //Specific number of decimals (rounds the next decimal numbers after those 2 in .toFixed(2))
